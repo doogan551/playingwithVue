c63393c2a72d70c2f6f1f7044d08696db83a3c8e
Rob Kendall <robert.ian.kendall@gmail.com>
2016-02-11 11:21:16 -0500

    configuring service installer adding command line arguments module

--- apps/service.js
+++ apps/service.js
@@ -1,22 +1,39 @@
 var Service = require('node-windows').Service;
+var commandLineArgs = require('command-line-args');
+
+var cli = commandLineArgs([{
+  name: 'install',
+  alias: 'i',
+  type: Boolean
+}, {
+  name: 'uninstall',
+  alias: 'u',
+  type: Boolean
+}]);
+
+var options = cli.parse();
 
 // Create a new service object
 var svc = new Service({
-  name:'InfoscanJS',
+  name: 'InfoscanJS',
   description: 'Infoscan web server.',
-  script: 'C:/infoscanjs/app.js'
+  script: require('path').join(__dirname, '..', 'app.js')
 });
 
 // Listen for the "install" event, which indicates the
 // process is available as a service.
-svc.on('install',function(){
+svc.on('install', function() {
+  console.log('Installed', svc.description);
   svc.start();
 });
 
-svc.on('uninstall',function(){
+svc.on('uninstall', function() {
   console.log('Uninstall complete.');
-  console.log('The service exists: ',svc.exists);
+  console.log('The service exists: ', svc.exists);
 });
 
-// Uninstall the service.
-svc.uninstall();
+if (!!options.uninstall) {
+  svc.uninstall();
+} else {
+  svc.install();
+}
