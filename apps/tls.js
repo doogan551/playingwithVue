e0c2cadfb7dba778a8cde9af0208b771b002ee35
Rob Kendall <robert.ian.kendall@gmail.com>
2016-02-17 10:31:24 -0500

    adding config and certs from linux box

--- apps/tls.js
+++ apps/tls.js
@@ -2,28 +2,31 @@ var lex = require('letsencrypt-express').testing();
 var express = require('express');
 var app = express();
 
-app.use('/', function (req, res) {
-  res.send({ success: true });
+app.use('/', function(req, res) {
+  res.send({
+    success: true
+  });
 });
 
 lex.create({
-  configDir: '../lib/letsencrypt.config'                 // ~/letsencrypt, /etc/letsencrypt, whatever you want
-
-, onRequest: app                                    // your express app (or plain node http app)
-
-, letsencrypt: null                                 // you can provide you own instance of letsencrypt
-                                                    // if you need to configure it (with an agreeToTerms
-                                                    // callback, for example)
-
-, approveRegistration: function (hostname, cb) {    // PRODUCTION MODE needs this function, but only if you want
-                                                    // automatic registration (usually not necessary)
-                                                    // renewals for registered domains will still be automatic
+  configDir: '../lib/letsencrypt.config', // ~/letsencrypt, /etc/letsencrypt, whatever you want
+  onRequest: app, // your express app (or plain node http app)
+  letsencrypt: null, // you can provide you own instance of letsencrypt
+  // if you need to configure it (with an agreeToTerms
+  // callback, for example)
+  approveRegistration: function(hostname, cb) { // PRODUCTION MODE needs this function, but only if you want
+    // automatic registration (usually not necessary)
+    // renewals for registered domains will still be automatic
     cb(null, {
-      domains: ['66.226.33.45', 'localhost']
-    , email: 'rkendall@dorsett-tech.com'
-    , agreeTos: true              // you 
+      domains: ['dorsett.duckdns.org'],
+      email: 'rkendall@dorsett-tech.com',
+      agreeTos: true // you 
     });
   }
-}).listen([80], [443, 5001], function () {
+}).listen([80], [443], function() {
   console.log("ENCRYPT __ALL__ THE DOMAINS!");
 });
+
+// ./letsencrypt-auto certonly --standalone --email rkendall@dorsett-tech.com -d dorsett.duckdns.org
+
+// /etc/letsencrypt/live/dorsett.duckdns.org/fullchain.pem
