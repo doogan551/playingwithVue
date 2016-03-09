5d6971374e77fa76e065dffa8242ab77b89221cb
Rob Kendall <robert.ian.kendall@gmail.com>
2016-03-09 16:05:27 -0500

    tracking my config file

---
+++ config/rob.js
@@ -0,0 +1,26 @@
+var rob = {
+  'Infoscan': {
+    'dbConfig': {
+      'host': '192.168.1.88',
+      'dbName': 'infoscan'
+    },
+    'zmqConfig': {
+      'server': '192.168.1.88',
+    },
+    'files': {
+      'driveLetter': 'C'
+    },
+    'location': {
+      'site': 'Rob',
+      'email': 'rkendall@dorsett-tech.com'
+    }
+  }
+};
+
+rob.Infoscan.files.firmwareLocation = "\\\\192.168.1.88/d$/InfoScan/Firmware/";
+
+rob.redisConfig = {
+  host: rob.Infoscan.dbConfig.host
+};
+
+module.exports = rob;
