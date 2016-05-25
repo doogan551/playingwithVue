17f3d7e1693053fb68b6fc3eb650957df6d1a758
Stephen Trent <stephen@DI.local>
2016-05-25 12:58:51 -0400

    Workspace v2 initial updates.

---
+++ public/js/v2/home.js
@@ -0,0 +1,23 @@
+var dti = {
+    clock: {
+        $el: $('.taskbar > .clock'),
+        checkTime: function (initial) {
+            var now = moment(),
+                setText = function () {
+                    dti.clock.$el.html(now.format('HH:mm'));
+                };
+
+            setText();
+
+            if (!dti.clock.hasReset && now.seconds() === 0) {
+                dti.clock.hasReset = true;
+                dti.clock.interval = window.setInterval(dti.clock.checkTime, 1000 * 60);
+            } else {
+                setTimeout(function () {
+                    dti.clock.checkTime();
+                }, (60 - now.seconds()) * 1000 - now.milliseconds());
+            }
+        }
+    }
+};
+
