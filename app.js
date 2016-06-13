a687713ceb1c08fe2117165156ef22f10789a03d
rob <rkendall@dorsett-tech.com>
2016-06-13 13:43:39 -0400

    removing mongoose junk and pg was installed for some reason.

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ require('./helpers/passport')(passport); // pass passport for configuration
 
 //if production, use dist folders
 if (config.minifyFiles !== false) {
-    //no need to test for existence of directories, it falls back to public/...
-    logger.info('Using minified files');
-    app.use('/js', express.static(__dirname + '/dist/public/js'));
-    app.use('/css', express.static(__dirname + '/dist/public/css'));
+  //no need to test for existence of directories, it falls back to public/...
+  logger.info('Using minified files');
+  app.use('/js', express.static(__dirname + '/dist/public/js'));
+  app.use('/css', express.static(__dirname + '/dist/public/css'));
 }
 
 app.use(express.static(__dirname + '/public'));
@@ -62,17 +62,17 @@ app.set('view engine', 'jade');
 
 //if production, use dist folders
 if (config.minifyFiles !== false) {
-    try {
-        var stat = fs.statSync(__dirname + '/dist/views/');
-        if (stat.isDirectory()) {
-            logger.info('Found prod directory, redirecting views');
-            app.set('views', __dirname + '/dist/views');
-        } else {
-            logger.info('Prod view folder not found, skipping view redirect');
-        }
-    } catch (ex) {
-        logger.info('Prod view folder not found, skipping view redirect');
+  try {
+    var stat = fs.statSync(__dirname + '/dist/views/');
+    if (stat.isDirectory()) {
+      logger.info('Found prod directory, redirecting views');
+      app.set('views', __dirname + '/dist/views');
+    } else {
+      logger.info('Prod view folder not found, skipping view redirect');
     }
+  } catch (ex) {
+    logger.info('Prod view folder not found, skipping view redirect');
+  }
 }
 
 
@@ -91,52 +91,44 @@ app.use(passport.session());
 
 app.use('/', require('./helpers/router')(_controllers));
 
-require('./helpers/mongooseconn.js')(function() {
-  db.connect(connectionString.join(''), function(err) {
-    if (err) {
-      logger.error('unable to connect');
-      process.exit(1);
-    } else {
-      logger.info('mongo connected to', connectionString.join(''));
-      require('./helpers/processes')(function(err) {
-        logger.info('processes', err);
-        require('./helpers/globals').setGlobals(function() {
-          if (!!config.get('Infoscan.letsencrypt').enabled) {
+db.connect(connectionString.join(''), function(err) {
+  require('./helpers/processes')(function(err) {
+    logger.info('processes', err);
+    require('./helpers/globals').setGlobals(function() {
+      if (!!config.get('Infoscan.letsencrypt').enabled) {
 
-            var lex = LEX.create({
-              configDir: config.get('Infoscan.files').driveLetter + ':' + config.get('Infoscan.letsencrypt').directory,
-              approveRegistration: function(hostname, cb) {
-                cb(null, {
-                  domains: config.get('Infoscan').domains,
-                  email: 'rkendall@dorsett-tech.com', // 'user@example.com'
-                  agreeTos: true
-                });
-              }
-            });
-
-            http.createServer(LEX.createAcmeResponder(lex, function redirectHttps(req, res) {
-              res.writeHead(301, {
-                "Location": "https://" + req.headers['host'] + req.url
-              });
-              res.end();
-            })).listen(80);
-
-            var httpsServer = https.createServer(lex.httpsOptions, LEX.createAcmeResponder(lex, app));
-            sockets.connect(config, httpsServer, sessionStore, cookieParser, function() {
-              require('./socket/common').socket();
-              httpsServer.listen(443);
-            });
-          } else {
-            var httpServer = http.createServer(app);
-            sockets.connect(config, httpServer, sessionStore, cookieParser, function() {
-              require('./socket/common').socket();
-              httpServer.listen(80);
+        var lex = LEX.create({
+          configDir: config.get('Infoscan.files').driveLetter + ':' + config.get('Infoscan.letsencrypt').directory,
+          approveRegistration: function(hostname, cb) {
+            cb(null, {
+              domains: config.get('Infoscan').domains,
+              email: 'rkendall@dorsett-tech.com', // 'user@example.com'
+              agreeTos: true
             });
           }
-
         });
-      });
-    }
+
+        http.createServer(LEX.createAcmeResponder(lex, function redirectHttps(req, res) {
+          res.writeHead(301, {
+            "Location": "https://" + req.headers['host'] + req.url
+          });
+          res.end();
+        })).listen(80);
+
+        var httpsServer = https.createServer(lex.httpsOptions, LEX.createAcmeResponder(lex, app));
+        sockets.connect(config, httpsServer, sessionStore, cookieParser, function() {
+          require('./socket/common').socket();
+          httpsServer.listen(443);
+        });
+      } else {
+        var httpServer = http.createServer(app);
+        sockets.connect(config, httpServer, sessionStore, cookieParser, function() {
+          require('./socket/common').socket();
+          httpServer.listen(80);
+        });
+      }
+
+    });
   });
 });
 
