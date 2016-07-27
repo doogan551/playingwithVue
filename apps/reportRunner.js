15aafb93b79dcb2b89fc16ba75a22ea0adc2c72a
rob <rkendall@dorsett-tech.com>
2016-07-27 13:36:36 -0400

    files to run phantomjs

--- apps/reportRunner.js
+++ apps/reportRunner.js
@@ -1,2 +1,9 @@
-console.log('Hello, world!');
-phantom.exit();
+var page = require('webpage').create();
+page.open('http://google.com/', function(status) {
+  console.log("Status: " + status);
+  if(status === "success") {
+    page.render('example.pdf');
+  }
+
+  phantom.exit();
+});
