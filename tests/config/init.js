29ea183ca611f1d598a7d915ea4f03c24801061b
Rob <robert.ian.kendall@gmail.com>
2016-10-27 13:15:34 -0400

    - setting up config and AI files for tests

---
+++ tests/config/init.js
@@ -0,0 +1,27 @@
+/* global global, process, chai */
+// process.env.NODE_ENV = 'tester';
+console.log(process.env.NODE_ENV);
+var db = require('../../helpers/db');
+var _ = require('lodash');
+var config = require('config');
+var dbConfig = config.get('Infoscan.dbConfig');
+var connectionString = [dbConfig.driver, '://', dbConfig.host, ':', dbConfig.port,
+  '/tester'
+];
+
+before(function(done) {
+
+  global.chai = require('chai');
+  global.expect = chai.expect;
+
+  global.adminUser = {
+    "System Admin": {
+      "Value": true
+    },
+    groups: []
+  };
+  global._ = _;
+  console.log('running before mocha init', connectionString.join(''));
+  // db.connect(connectionString.join(''), done);
+  done();
+});
