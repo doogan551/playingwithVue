4947f9a9825988a98b95542c242f5c0df94b9574
Rob Kendall <rkendall@dorsett-tech.com>
2016-11-01 15:49:41 -0400

    - adding file to dump and restore a mongodb

---
+++ apps/moveMongoDB.js
@@ -0,0 +1,72 @@
+var config = require('config');
+var process = require('child_process').exec;
+var moment = require('moment');
+var dbModel = require('../helpers/db');
+var Utility = require('../models/utility');
+var dbConfig = config.get('Infoscan.dbConfig');
+var fromHost = "";
+var fromDB = "";
+var toHost = dbConfig.host;
+var toDB = dbConfig.dbName;
+var date = moment().format('YYYYMMDDX');
+var folder = config.get('Infoscan.files').driveLetter + ':/mongodump/' + date;
+
+var Service = require('node-windows').Service;
+var commandLineArgs = require('command-line-args');
+
+var cli = commandLineArgs([{
+  name: 'fromHost',
+  alias: 'h',
+  type: String
+}, {
+  name: 'fromDB',
+  alias: 'd',
+  type: String
+}, {
+  name: 'toHost',
+  alias: 't',
+  type: String
+}, {
+  name: 'toDB',
+  alias: 'u',
+  type: String
+}]);
+
+var options = cli.parse();
+
+if (!options.fromHost) {
+  console.error('You must supply a host to retrieve from');
+  process.exit(1);
+} else if (!options.fromDB) {
+  console.error('You must supply a database to retrieve from');
+  process.exit(1);
+} else {
+  fromHost = options.fromHost;
+  fromDB = options.fromDB;
+}
+
+if (options.toHost) {
+  toHost = options.toHost;
+}
+if (options.toDB) {
+  toDB = options.toDB;
+}
+
+child = process('"' + config.get('Infoscan.files').driveLetter + ':/Program Files/MongoDB/Server/3.2/bin/mongodump.exe" -h ' + fromHost + ' -d ' + fromDB + ' -o ' + folder, function(err, stdout, stderr) {
+  logResults('mongodump out:', err, stdout, stderr);
+  child = process('"' + config.get('Infoscan.files').driveLetter + ':/Program Files/MongoDB/Server/3.2/bin/mongorestore.exe" -h ' + toHost + ' -d ' + toDB + ' ' + folder + '/' + fromDB, function(err, stdout, stderr) {
+    logResults('git reset out:', err, stdout, stderr);
+  });
+});
+
+
+
+function logResults(msg, err, stdout, stderr) {
+  console.log(msg);
+  console.log(err);
+  console.log('------');
+  console.log(stdout.toString());
+  console.log('------');
+  console.log(stderr.toString());
+  console.log('######');
+}
