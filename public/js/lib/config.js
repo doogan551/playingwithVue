cd39b7727ec6995f44b1af8a8a1ef52b5810e791
Morris <mhealy@dorsett-tech.com>
2016-12-15 11:15:59 -0500

    Fix typo

--- public/js/lib/config.js
+++ public/js/lib/config.js
@@ -3648,7 +3648,7 @@ var Config = (function(obj) {
                 point["Ethernet Gateway"].isReadOnly = ro;
                 point["Ethernet Subnet"].isReadOnly = ro;
                 point["Ethernet  IP Port"].isReadOnly = (point["Ethernet Network"].Value !== 0) ? true : false;
-                point.["Ethernet Network"].isDisplayable = true;
+                point["Ethernet Network"].isDisplayable = true;
                 setDisp(point, ["Ethernet Address", "Ethernet IP Port", "Ethernet Gateway", "Ethernet Subnet"], obj.Utility.checkMicroScan5Device(point));
             } else {
                 setDisp(point, ["Ethernet Address", "Ethernet IP Port", "Ethernet Gateway", "Ethernet Subnet", "Ethernet Network"], false);
