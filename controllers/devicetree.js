426dc5b41986564eb2dc955dc61e50107358d949
Rob Kendall <robert.ian.kendall@gmail.com>
2017-04-27 09:00:28 -0400

    device tree was caching results

--- controllers/devicetree.js
+++ controllers/devicetree.js
@@ -2,14 +2,15 @@ let express = require('express');
 let router = express.Router();
 let _ = require('lodash');
 let utils = require('../helpers/utils.js');
-let DeviceTree = new(require('../models/devicetree'))();
+let DeviceTree = require('../models/devicetree');
 
 // POSTMAN
 router.get('/gettree', function (req, res, next) {
+    let deviceTreeModel = new DeviceTree();
     let data = _.merge(req.params, req.body);
     data.user = req.user;
 
-    DeviceTree.getTree(function (err, result) {
+    deviceTreeModel.getTree(function (err, result) {
         if (err) {
             return utils.sendResponse(res, {
                 err: err
