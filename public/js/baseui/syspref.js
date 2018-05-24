84c330d934a284cd1177b5751f58e7b6ca520ce4
Rob Kendall <rkendall@dorsett-tech.com>
2018-05-24 10:18:33 -0400

    setting dirty to false if season isn't updated

--- public/js/baseui/syspref.js
+++ public/js/baseui/syspref.js
@@ -383,6 +383,7 @@ var calendarViewModel = function () {
             vm = viewModel;
 
             if (viewModel.season() === viewModel.originalSeason) {
+                vm.dirty(false);
                 return;
             }
             data = {
